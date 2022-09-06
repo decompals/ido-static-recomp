19a60500528f341632ac672184953f27071b56e2
Derek Hensley <hensley.derek58@gmail.com>
2022-09-05 21:10:33 -0700

    progress on as0 support

--- libc_impl.c
+++ libc_impl.c
@@ -269,6 +269,13 @@ static void find_bin_dir(void) {
 #endif
 }
 
+void clean(uint8_t *mem) {
+    wrapper_fflush(mem, 0);
+    free_all_file_bufs(mem);
+    mem += MEM_REGION_START;
+    memory_unmap(mem, MEM_REGION_SIZE);
+}
+
 int main(int argc, char *argv[]) {
     int ret;
 
@@ -278,10 +285,7 @@ int main(int argc, char *argv[]) {
     mem -= MEM_REGION_START;
     int run(uint8_t *mem, int argc, char *argv[]);
     ret = run(mem, argc, argv);
-    wrapper_fflush(mem, 0);
-    free_all_file_bufs(mem);
-    mem += MEM_REGION_START;
-    memory_unmap(mem, MEM_REGION_SIZE);
+    clean(mem);
     return ret;
 }
 
@@ -766,7 +770,12 @@ int wrapper_fprintf(uint8_t *mem, uint32_t fp_addr, uint32_t format_addr, uint32
     }*/
     // Special-case this one format string. This seems to be the only one that uses `%f` or width specifiers.
     if (!strcmp(format, "%.2fu %.2fs %u:%04.1f %.0f%%\n") && fp_addr == STDERR_ADDR) {
-        fprintf(stderr, format, MEM_U32(sp + 0), MEM_U32(sp + 4), MEM_U32(sp + 8), MEM_U32(sp + 12), MEM_U32(sp + 16));
+        float arg0 = MEM_F32(sp + 0);
+        float arg1 = MEM_F32(sp + 8);
+        float arg2 = MEM_U32(sp + 16);
+        float arg3 = MEM_F32(sp + 24);
+        float arg4 = MEM_F32(sp + 32);
+        fprintf(stderr, format, arg0, arg1, arg2, arg3, arg4);
         fflush(stderr);
         return 1;
     }
@@ -2182,6 +2191,7 @@ void wrapper_abort(uint8_t *mem) {
 }
 
 void wrapper_exit(uint8_t *mem, int status) {
+    clean(mem); //! TODO: is this what we want to do?
     exit(status);
 }
 
