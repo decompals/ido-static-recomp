fd10a6fa3bebce68d0628ab758ea33dc0e09ffbd
Elliptic Ellipsis <elliptic.ellipsis@gmail.com>
2022-09-27 21:44:12 +0100

    git subrepo clone (merge) git@github.com:Decompollaborate/rabbitizer.git tools/rabbitizer
    subrepo:
      subdir:   "tools/rabbitizer"
      merged:   "70426d5"
    upstream:
      origin:   "git@github.com:Decompollaborate/rabbitizer.git"
      branch:   "master"
      commit:   "70426d5"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "b8b195cc6"

---
+++ tools/rabbitizer/include/instructions/RabbitizerInstrId.h
@@ -0,0 +1,37 @@
+/* SPDX-FileCopyrightText: © 2022 Decompollaborate */
+/* SPDX-License-Identifier: MIT */
+
+#ifndef RABBITIZER_INSTRID_H
+#define RABBITIZER_INSTRID_H
+#pragma once
+
+#include "common/Utils.h"
+
+
+#define RABBITIZER_DEF_INSTR_ID(prefix, caseBits, name, ...) \
+    RABBITIZER_INSTR_ID_##prefix##_##name,
+
+#define RABBITIZER_DEF_INSTR_ID_ALTNAME(prefix, caseBits, name, altname, ...) \
+    RABBITIZER_DEF_INSTR_ID(prefix, caseBits, name, __VA_ARGS__)
+
+typedef enum RabbitizerInstrId {
+    #include "instructions/instr_id/RabbitizerInstrId_cpu.inc"
+
+    #include "instructions/instr_id/RabbitizerInstrId_rsp.inc"
+
+    #include "instructions/instr_id/RabbitizerInstrId_r5900.inc"
+
+    RABBITIZER_INSTR_ID_ALL_MAX = RABBITIZER_DEF_INSTR_ID(r5900, , MAX, )
+} RabbitizerInstrId;
+
+#undef RABBITIZER_DEF_INSTR_ID
+#undef RABBITIZER_DEF_INSTR_ID_ALTNAME
+
+
+extern const char *RabbitizerInstrId_Names[];
+
+
+CONST NODISCARD RETURNS_NON_NULL
+const char *RabbitizerInstrId_getOpcodeName(RabbitizerInstrId uniqueId);
+
+#endif
